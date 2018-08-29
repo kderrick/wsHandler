5918a6402afe883bcef3ca57a76ec50a9fb06e18
Kyle <kderrick1988@gmail.com>
2018-08-29 16:47:00 -0700

    WIP: implement handleMessage in SiteHandler

--- src/main/java/com/kyle/trainings/ws/handlers/SiteHandler.java
+++ src/main/java/com/kyle/trainings/ws/handlers/SiteHandler.java
@@ -1,8 +1,13 @@
 package com.kyle.trainings.ws.handlers;
 
+import java.util.Iterator;
 import java.util.Set;
 
 import javax.xml.namespace.QName;
+import javax.xml.soap.SOAPEnvelope;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPHeader;
+import javax.xml.soap.SOAPMessage;
 import javax.xml.ws.handler.MessageContext;
 import javax.xml.ws.handler.soap.SOAPHandler;
 import javax.xml.ws.handler.soap.SOAPMessageContext;
@@ -11,25 +16,36 @@ public class SiteHandler implements SOAPHandler<SOAPMessageContext> {
 
 	@Override
 	public boolean handleMessage(SOAPMessageContext context) {
-		// TODO Auto-generated method stub
+		
+		Boolean isResponse = (Boolean) context.get(MessageContext.MESSAGE_OUTBOUND_PROPERTY);
+		if (!isResponse) {
+			SOAPMessage soapMessage = context.getMessage();
+			try {
+				SOAPEnvelope soapEnvelope = soapMessage.getSOAPPart().getEnvelope();
+				SOAPHeader soapHeader = soapEnvelope.getHeader();
+				Iterator childElements = soapHeader.getChildElements();
+			} catch (SOAPException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("Response on the way");
+		}
 		return false;
 	}
 
 	@Override
 	public boolean handleFault(SOAPMessageContext context) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public void close(MessageContext context) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public Set<QName> getHeaders() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
